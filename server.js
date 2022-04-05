6d97115147774fffa2038706ea83f6c9727032e5
Vinay Karthik Maradi Balachandra <72756353+vkvinay580@users.noreply.github.com>
2022-04-05 11:53:07 +0200

    Add Heroku url

---
+++ server.js
@@ -0,0 +1,19 @@
+const server = require('http').createServer()
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "https://tictactoevk.herokuapp.com/",
+        //origin:"http://localhost:8080",
+        methods: ["GET", "POST"]
+    }
+});
+io.on('connection', (socket)=> {
+    socket.emit("hello", "tic-tac-toe game");
+    socket.on("play", index => {
+        console.log("server received", index)
+        socket.broadcast.emit("play", index)
+        ;
+    })
+})
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT);
